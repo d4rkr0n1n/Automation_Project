6d49c6e320db886776a0fa46b23bfe2ddadc2045
Mridul Roy <midlry.mr@gmail.com>
2023-05-27 12:13:59 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Automation_Project
 
+### Automation-v0.1
 1. Create/Update variables
 2. Update packages & install awscli and apache2
 3. Enable and start the service apache2
 4. Create tar of log file in /tmp directory
 5. Upload the tar to s3 bucket
+
+### Automation-v0.2
+1. Bookkeeping feature to record the log tar file uploads to s3 bucket
+2. Added cronjob
--- automation.sh
+++ automation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 myname=Mridul
 timestamp=$(date '+%d%m%Y-%H%M%S')
 s3_bucket=upgrad-mridul
@@ -11,6 +10,22 @@ sudo systemctl enable --now apache2
 
 ufw allow 80
 
-tar -cvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/
+tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/
 
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+if [ $(ls /var/www/html | grep inventory.html | wc -l) == 0 ]
+then
+        echo "<b>Log Type &emsp;  Date Created &emsp;&emsp;&emsp;  Type &emsp;  Size</b><br>" > /var/www/html/inventory
+else
+        echo "inventory.html already exists"
+fi
+
+echo -e "http-logs &emsp; ${timestamp} &emsp;&emsp;&nbsp; tar &emsp;&nbsp;&nbsp $(ls -sh /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}')<br>" >> /var/www/html/inventory.html
+
+if [ $(crontab -l | grep "* * * * * root /root/Automation_Project/automation.sh" | wc -l) == 0 ]
+then
+        echo "* * * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+else
+        echo "crontab already exists"
+fi
